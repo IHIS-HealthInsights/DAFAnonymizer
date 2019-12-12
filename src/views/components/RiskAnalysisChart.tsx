f5fa9db46fc8105c3a9e8462a165dbf229d5257a
clarencenpy <clarencenpy@gmail.com>
2019-12-13 01:05:30 +0800

    Preview records when clicking on risk analysis chart

--- src/views/components/RiskAnalysisChart.tsx
+++ src/views/components/RiskAnalysisChart.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { ResponsiveLine } from "@nivo/line";
 
 const RiskAnalysisChart = ({ data, setPreviewRiskRecordsK }) => (
