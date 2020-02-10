04988caa566ea464fe8256b8dadc571e6552bd11
clarencenpy <clarencenpy@gmail.com>
2020-02-10 10:22:59 +0800

    Update SHI list

--- src/anonymizer/Matchers.ts
+++ src/anonymizer/Matchers.ts
@@ -23,9 +23,14 @@ export class NricMatcher implements Matcher {
 
 export class SHIMatcher implements Matcher {
   regexes = [
+    // Case Sensitive
     /\b(AIDS|VDRL|RPR|TPPA|TPHA|HSV)\b/g,
-    /\b(abuse|addiction|alcoholism|cannabis|hiv)\b/gi
+    // Case Insensitive
+    /\b(Abuse|Addiction|Alcoholic|Alcoholism|Amphetamine|Cannabis|Chancre|Chancroid|Chlamyd|Cocaine|Dependence|Ducreyi|Ecstasy|Gonococc|Gonor|Heroin|HIV|Marijuana|Opium|Pallid|Reagin|Retroviral|Spiroch|Sterilisation|Sterilization|Syphilis|Treponema|Trichomon|Urealyticum|Ureaplasma|Venereal|Withdraw|Tetraplex)\b/gi,
+    // Multiple words
+    /\b(C Trach|M genital|M hominis|N Gonor|Genital herpes|Herpes genital|Infect genital|Genital infect|Human immun|Sexual transmit|Transmit sexual|Herpes simple)\b/gi
   ];
+
   description = "Sensitive Health Information";
   match(text) {
     let matches = [];
