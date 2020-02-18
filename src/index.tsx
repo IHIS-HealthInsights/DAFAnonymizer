3b3a7f76900af5a1aaf60572dc41bed3db2a401f
clarencenpy <clarencenpy@gmail.com>
2020-02-18 17:43:22 +0800

    Preload service worker when application starts

--- src/index.tsx
+++ src/index.tsx
@@ -10,3 +10,21 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Pre-install service worker
+const src = "./install_service_worker.html";
+const iframe = document.createElement("iframe");
+(iframe as any).loaded = false;
+iframe.hidden = true;
+iframe.src = src;
+iframe.name = "iframe";
+(iframe as any).postMessage = (...args) =>
+  (iframe.contentWindow.postMessage as any)(...args);
+iframe.addEventListener(
+  "load",
+  () => {
+    (iframe as any).loaded = true;
+  },
+  { once: true }
+);
+document.body.appendChild(iframe);
